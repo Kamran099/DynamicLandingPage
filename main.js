f1f3749fbe43646a0f89c12dc06033a728137cc3
Kamran <kamran-anwar99@hotmail.com>
2022-06-27 19:49:04 +0100

    Added number 0 function for seconds

--- main.js
+++ main.js
@@ -20,11 +20,38 @@ function showTime() {
     hour = hour % 12 || 12;
 
     //Output Time
-    time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
 
     setTimeout(showTime, 1000);
 }
 
+// Add Zeros
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set BG and greeting
+function setBgGreet() {
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12) {
+        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+        greeting.textContent = 'Good Morning, ';
+    } else if (hour < 18) {
+        // Afternoon
+        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon, ';
+    } else {
+        // Evening
+        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        greeting.textContent = 'Good Evening, ';
+        document.body.style.color = 'white';
+    }
+}
+
 //Run
 showTime();
+setBgGreet();
 
